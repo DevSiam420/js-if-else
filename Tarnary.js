8ba195629bee75f36d648b18bf856f7021508276
DevSiam420 <siam.molla.dev420@gmail.com>
2025-10-03 12:28:13 +0600

    learn tarnar operator

---
+++ Tarnary.js
@@ -0,0 +1,16 @@
+const age = 18 ;
+//   tarnary operator 
+const vote = (age >= 18) ? "tumi vote dio" : "tumi vote diba na " ;
+ console.log(vote);
+
+// if else operator 
+
+
+    if( age >= 18 ){
+        console.log("vote dio");
+
+    }
+    else{
+        console.log("vote dibana ");
+        
+    }
