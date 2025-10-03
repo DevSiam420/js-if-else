0f67c2f522bcc17da75aa133edb87f4b9b82c2cb
DevSiam420 <siam.molla.dev420@gmail.com>
2025-10-03 10:30:16 +0600

    learn if else

--- keys.js
+++ keys.js
@@ -10,3 +10,24 @@ if ( (salary >= 23000 || height > 60) && isBcs == true){
 else{
   console.log("vagg sala")
 }
+
+const price = 3000 ;
+ 
+if (price >= 5000){
+  // 10 % discount ======
+  discount = price * 10 / 100 ;
+  console.log(discount);
+   payamount = (price - discount);
+   console.log(payamount);
+
+}
+
+else if ( price > 2500 ){
+  // 5 % discount 
+  const discount = price * 5 / 100 ;
+  console.log(discount);
+
+  const payamount = price - discount;
+  console.log(payamount);
+
+}
